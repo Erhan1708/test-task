8688eb6061bb7cc07d5d797b6ca37b6295dc6c3f
Erhan <erhankubanych@gmail.com>
2022-10-24 23:17:46 +0600

    feat: code completed

--- script.js
+++ script.js
@@ -1,66 +1,27 @@
-const data = [
-   {
-      id: 1,
-      name: "Категория - 1",
-      categories: [
-         {
-            id: 2,
-            name: "Категория - 2",
-            categories: null
-         },
-         {
-            id: 3,
-            name: "Категория - 3",
-            categories: [
-               {
-                  id: 4,
-                  name: "Категория - 4",
-                  categories: null
-               }
-            ]
-         },
-         {
-            id: 5,
-            name: "Категория - 5",
-            "categories": null
-         },
-         {
-            id: 6,
-            name: "Категория - 6",
-            categories: null
-         }
-      ]
-   },
-   {
-      id: 7,
-      name: "Категория - 7",
-      categories: [
-         {
-            id: 8,
-            name: "Категория - 8",
-            categories: null
-         }
-      ]
-   },
-   {
-      id: 9,
-      name: "Категория - 9",
-      categories: null
-   }
-]
+ const getCategories = async (url) => {
+	const response = await fetch(url);
 
+	if (!response.ok) {
+		throw new Error(`Ошибка по адресу ${url}, статус ошибки ${response.status}`)
+	}
 
-const test=(data)=> {
-   let result = [];
-
-   data.forEach(el => {
-      result.push(el.name);
-
-      if (el.categories)
-         result.push(...test(el.categories));
-   });
-
-   return result;
+	return await response.json();
 }
 
-console.log(test(data));
+getCategories('http://localhost:3001/hydra-member').then(data => {
+   const Categories = data.flatMap( function recursive(el){
+      return (`
+      <ul><li class="post">${ [el.name].concat(el.categories?.flatMap(recursive) || [])}</li></ul>
+      
+      `
+      )
+   })
+   
+   
+
+	Categories.forEach(e => {
+		let categories = document.querySelector(".categories")
+      categories.innerHTML = categories.innerHTML + e.replace(/[\,]/g, '');
+	})
+})
+	.catch(err => console.error(err))
